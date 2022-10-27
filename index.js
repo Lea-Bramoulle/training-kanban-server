694403df6c4accf021dd8ee8f71d5a008573bcff
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-10-27 19:40:27 +0200

    :lock: feat : add bodySanitizer + cors security

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const sanitizer = require('./app/middlewares/bodySanitizer');
 
 const app = express();
 
@@ -6,6 +9,16 @@ const router = require('./app/routers/index');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const upload = multer();
+app.use(upload.none());
+
+app.use(sanitizer);
+
+// app.use(cors({
+//     origin:"http://localhost:5000"
+// }));
+app.use(cors('*'));
 
 app.use(router);
 
