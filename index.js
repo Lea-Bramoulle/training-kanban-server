be32af175a92bc91166bd53343dcdd87678ff6a1
Lea-Bramoulle <l.bramoulle@hotmail.fr>
2022-10-27 19:59:47 +0200

    :rocket: feat: add limit rate to prevent server overload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+const rateLimit = require('express-rate-limit');
+
 const sanitizer = require('./app/middlewares/bodySanitizer');
 
 const app = express();
@@ -14,6 +16,15 @@ const upload = multer();
 app.use(upload.none());
 
 app.use(sanitizer);
+
+// Add rate limit policy
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100000, // Limit each IP to 100K requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 
 // app.use(cors({
 //     origin:"http://localhost:5000"
